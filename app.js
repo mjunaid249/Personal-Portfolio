e63fef3bbc2ac52d84977a65ca12ba958c1e9e72
mjunaid249 <105982589+mjunaid249@users.noreply.github.com>
2023-10-15 17:05:37 +0500

    Upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let header = document.querySelector(".header");
+
+let menu_btn = document.querySelector(".mobile-navbar-btn");
+
+menu_btn.addEventListener("click", () => {
+  header.classList.toggle("active");
+});
+var typed = new Typed("#element", {
+  strings: ["Web Developer", "Frontend Developer", "React Developer"],
+  typeSpeed: 60,
+  loop: true,
+});
+var typed = new Typed("#e-2", {
+  strings: ["Web Developer", "Frontend Developer", "React Developer"],
+  typeSpeed: 60,
+  loop: true,
+});
